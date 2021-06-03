10b92213eb567fb4b09f8b0a39240cbef65b9cb5
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-03 21:32:44 +0530

    Update greetUser.js

--- src/functions/greetUser.js
+++ src/functions/greetUser.js
@@ -3,9 +3,10 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
-const greetUser = function (guild, member) {
+const getGuild = require("../db/functions/getGuild");
+const greetUser = asnyc (guild, member) => {
     let channel;
-    let msg = `Welcome ${member} to the ${guild.name} server.`; //This string will be replaced to get from db afterwards
+    let msg = await getGuild(guild.id).welcomeMessage;
     //Replace Placeholders with their values
     msg = msg
         .replace("{mention}", `${member}`)
