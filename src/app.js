f804af4234e4980388383ebb6667f220da009791
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-06-03 19:45:53 +0530

    fix mongooseError

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
+const dotenv = require("dotenv").config();
+
 const Discord = require("discord.js");
 const dotenv = require("dotenv").config();
 
