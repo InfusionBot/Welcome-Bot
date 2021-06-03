76d89ff6ee23bef10dc1fcf30bcca4888480da62
PuneetGopinath <baalkrshna@gmail.com>
2021-06-03 15:25:38 +0530

    Add set-chan and get-chan commands

--- src/app.js
+++ src/app.js
@@ -64,15 +64,26 @@ client.on("message", function (message) {
                 //Test greetUser function
                 greetUser(message.guild, message.member);
                 break;
+            case "set-chan":
+                //Set welcome channel
+                updateGuild(message.guild.id, "welcomeChannel", args[0]);
+                message.reply("channel set to " + args[0]);
+                break;
+            case "get-chan":
+                //Get welcome channel
+                message.reply("Channel currently is set to " + getGuild(message.guild.id).welcomeChannel);
+                break;
             case "set-msg":
                 //Set welcome message
                 updateGuild(message.guild.id, "welcomeMessage", args.join(" "));
                 message.reply("message set to " + args.join(" "));
                 break;
             case "get-msg":
-            default:
-                //Set welcome message
+                //Get welcome message
                 message.reply("Message currently is set to " + getGuild(message.guild.id).welcomeMessage);
+                break;
+            default:
+                message.reply("Are you trying to run a command?\nI think you have a typo in the command.");
                 break;
         }
     }
