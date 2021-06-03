d6f6ac3f36d9388a117421e307679441587804ef
PuneetGopinath <baalkrshna@gmail.com>
2021-06-03 16:14:47 +0530

    Use execute function

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const Discord = require("discord.js");
 const presence = require("./functions/presence");
 const greetUser = require("./functions/greetUser");
 const serverCount = require("./functions/serverCount");
+const execute = require("./functions/execute");
 
 if (
     !process.env.BOT_TOKEN ||
@@ -23,8 +24,6 @@ if (
 }
 
 require("./db/connection");
-const updateGuild = require("./db/functions/updateGuild");
-const getGuild = require("./db/functions/getGuild");
 const addGuild = require("./db/functions/addGuild");
 const removeGuild = require("./db/functions/removeGuild");
 
@@ -68,47 +67,7 @@ client.on("message", function (message) {
         );
     }
     if (message.content.startsWith(prefix)) {
-        const commandBody = message.content.slice(prefix.length);
-        const args = commandBody.split(" ");
-        const command = args.shift().toLowerCase();
-        switch (command) {
-            case "ping":
-                message.reply(`Pong!`);
-                break;
-            case "test":
-                //Test greetUser function
-                greetUser(message.guild, message.member);
-                break;
-            case "set-chan":
-                //Set welcome channel
-                updateGuild(message.guild.id, "welcomeChannel", args[0]);
-                message.reply("channel set to " + args[0]);
-                break;
-            case "get-chan":
-                //Get welcome channel
-                message.reply(
-                    "Channel currently is set to " +
-                        getGuild(message.guild.id).welcomeChannel
-                );
-                break;
-            case "set-msg":
-                //Set welcome message
-                updateGuild(message.guild.id, "welcomeMessage", args.join(" "));
-                message.reply("message set to " + args.join(" "));
-                break;
-            case "get-msg":
-                //Get welcome message
-                message.reply(
-                    "Message currently is set to " +
-                        getGuild(message.guild.id).welcomeMessage
-                );
-                break;
-            default:
-                message.reply(
-                    "Are you trying to run a command?\nI think you have a typo in the command."
-                );
-                break;
-        }
+        execute(message);
     }
 });
 
