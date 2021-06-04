c6b867805644cdc9bb0d51e9b9cfe3fb7a9143ef
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-04 22:28:35 +0530

    Create prefix.js

---
+++ src/commands/prefix.js
@@ -0,0 +1,66 @@
+module.exports = {
+    name: "prefix",
+    aliases: [],
+    description: "Manage perfix for this server",
+    args: true,
+    execute(message, args) {
+      const updateGuild = require("../db/functions/updateGuild");
+      const getGuild = require("../db/functions/getGuild");
+        switch (args[0].toLowerCase()) {
+                    case "set":
+                        if (message.member.hasPermission("ADMINISTRATOR")) {
+                            //Set bot prefix
+                            updateGuild(
+                                message.guild.id,
+                                "prefix",
+                                args
+                                    .join(" ")
+                                    .replace(`${args[0]} `, "")
+                                    .replace(" ", "")
+                            ); //replace(" ", "") to replace empty space, there is no empty space in a prefix
+                            message.reply(
+                                "Prefix set to '" +
+                                    args
+                                        .join(" ")
+                                        .replace(`${args[0]} `, "")
+                                        .replace(" ", "") +
+                                    "' (without quotes)"
+                            );
+                        } else {
+                            message.reply(
+                                "Sorry, You don't have ADMINISTRATOR permission"
+                            );
+                        }
+                        break;
+                    case "get":
+                        //Get bot prefix
+                        message.reply(
+                            "Prefix in this server is set to '" +
+                                guildDB.prefix +
+                                "' (without quotes)"
+                        );
+                        break;
+                    case "reset":
+                        guildDB = await getGuild(message.guild.id);
+                        //Reset bot prefix
+                        if (message.member.hasPermission("ADMINISTRATOR")) {
+                            updateGuild(message.guild.id, "prefix", "!w");
+                            message.reply(
+                                "Prefix reset to '" +
+                                    guildDB.prefix +
+                                    "' (without quotes)"
+                            );
+                        } else {
+                            message.reply(
+                                "Sorry, You don't have ADMINISTRATOR permission"
+                            );
+                        }
+                        break;
+                    default:
+                        message.reply(
+                            "Are you trying to run a subcommand?\nI think you have a typo in the subcommand."
+                        );
+                        break;
+                }
+    },
+};
