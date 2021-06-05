55b0db41b623dcfc37e1c9f8703984f150e4a1a5
PuneetGopinath <baalkrshna@gmail.com>
2021-06-05 09:56:38 +0530

    Fix

--- src/app.js
+++ src/app.js
@@ -12,14 +12,15 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 
-const commandFolders = fs.readdirSync("./src/commands");
+const commandFolder = "./src/commands";
+const commandFolders = fs.readdirSync(commandFolder);
 
 for (const folder of commandFolders) {
     const commandFiles = fs
-        .readdirSync(`./commands/${folder}`)
+        .readdirSync(`${commandFolder}/${folder}`)
         .filter((file) => file.endsWith(".js"));
     for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`);
+        const command = require(`${commandFolder}/${folder}/${file}`);
         client.commands.set(command.name, command);
     }
 }
