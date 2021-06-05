d612fad40c2926001763662ffc227453fd4c0741
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-05 07:36:48 +0530

    Update prefix.js

--- src/commands/admin/prefix.js
+++ src/commands/admin/prefix.js
@@ -1,3 +1,8 @@
+/**
+ * Discord Welcome bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
 module.exports = {
     name: "prefix",
     aliases: [],
