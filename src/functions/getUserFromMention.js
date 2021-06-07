fa6e14ee665519b9ed5e3c79615c48e57ea8ac94
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-07 10:11:33 +0530

    Update getUserFromMention.js

--- src/functions/getUserFromMention.js
+++ src/functions/getUserFromMention.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
-module.exports = function (mention) {
+module.exports = function (mention, client) {
     // The id is the first and only match found by the RegEx.
     const matches = mention.match(/^<@!?(\d+)>$/);
 
