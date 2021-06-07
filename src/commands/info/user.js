ef59226feb68d533dfd795e0a3398eb2fc2f93d1
PuneetGopinath <baalkrshna@gmail.com>
2021-06-07 15:28:38 +0530

    Add user command

---
+++ src/commands/info/user.js
@@ -0,0 +1,26 @@
+/**
+ * Discord Welcome bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+module.exports = {
+    name: "user",
+    aliases: ["whois"],
+    description: "Get information about a user",
+    args: true,
+    usage: "[@mention]",
+    execute(message, args) {
+        const { MessageEmbed } = require("discord.js");
+        const getUserFromMention = require("../../functions/getUserFromMention.js");
+        const user = getUserFromMention(args[0]);
+        let msg = new MessageEmbed();
+        msg.setTitle(`${user.tag}`);
+        msg.setDescription(`Information about ${args[0]}`);
+        msg.setThumbnail(`${user.defaultAvatarURL}`);
+        msg.addField("ID:", `\`\`\`\n${user.id}\n\`\`\``);
+        msg.addField("Avatar URL:", `[url](${user.defaultAvatarURL})`);
+        msg.addField("Joined:", `Joined discord at ${user.createdAt}\n`);
+        msg.addField("Presence:", `${user.presence}`)
+        message.channel.send(msg);
+    },
+};
