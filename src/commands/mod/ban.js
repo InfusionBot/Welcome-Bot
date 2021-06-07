f6faec8785fd61a4b4a4fe92f76d47bb0446d771
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-07 08:53:53 +0530

    Update ban.js

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -9,8 +9,25 @@ module.exports = {
     description: "Ban a user.",
     permissions: ["MANAGE_SERVER"],
     args: true,
-    usage: "[@mention] (reason)",
+    usage: "[@mention] [reason]",
     execute(message, args) {
         const getUserFromMention = require("../../functions/getUserFromMention.js");
+        if (args.length < 2) {
+            return message.reply('Please mention the user you want to ban and specify a ban reason.');
+        }
+
+        const user = getUserFromMention(args[0]);
+        if (!user) {
+            return message.reply('Please use a proper mention if you want to ban someone.');
+        }
+
+        const reason = args.slice(1).join(' ');
+        try {
+            await message.guild.members.ban(user, { reason });
+        } catch (error) {
+            return message.channel.send(`Failed to ban **${user.tag}**: ${error}`);
+        }
+
+        return message.channel.send(`Successfully banned **${user.tag}** from the server!`);
     },
 };
