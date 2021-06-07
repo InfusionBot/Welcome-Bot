e8e064f073736de5008699903e791cf55ff10ff8
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-07 09:08:27 +0530

    Update unban.js

--- src/commands/mod/unban.js
+++ src/commands/mod/unban.js
@@ -9,8 +9,33 @@ module.exports = {
     description: "Unban a user.",
     permissions: ["MANAGE_SERVER"],
     args: true,
-    usage: "[@mention] (reason)",
+    usage: "[@mention] [reason]",
     execute(message, args) {
         const getUserFromMention = require("../../functions/getUserFromMention.js");
+        if (args.length < 2) {
+            return message.reply(
+                "Please mention the user you want to unban and specify a unban reason."
+            );
+        }
+
+        const user = getUserFromMention(args[0]);
+        if (!user) {
+            return message.reply(
+                "Please use a proper mention if you want to unban someone."
+            );
+        }
+
+        const reason = args.slice(1).join(" ");
+        try {
+            await message.guild.members.unban(user, reason);
+        } catch (error) {
+            return message.channel.send(
+                `Failed to unban **${user.tag}**: ${error}`
+            );
+        }
+
+        return message.channel.send(
+            `Successfully unbanned **${user.tag}** from the server!`
+        );
     },
 };
