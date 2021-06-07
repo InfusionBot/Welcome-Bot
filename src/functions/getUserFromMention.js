f7bcba15edcba035ff4d73e8c25eba14fc7695d5
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-07 08:59:06 +0530

    Update getUserFromMention.js

--- src/functions/getUserFromMention.js
+++ src/functions/getUserFromMention.js
@@ -4,15 +4,15 @@
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
 module.exports = function (mention) {
-    if (!mention) return;
+    // The id is the first and only match found by the RegEx.
+    const matches = mention.match(/^<@!?(\d+)>$/);
 
-    if (mention.startsWith("<@") && mention.endsWith(">")) {
-        mention = mention.slice(2, -1);
+    // If supplied variable was not a mention, matches will be null instead of an array.
+    if (!matches) return;
 
-        if (mention.startsWith("!")) {
-            mention = mention.slice(1);
-        }
+    // However, the first element in the matches array will be the entire mention, not just the ID,
+    // so use index 1.
+    const id = matches[1];
 
-        return client.users.cache.get(mention);
-    }
+    return client.users.cache.get(id);
 };
