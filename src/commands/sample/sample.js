bdb680507507f8cbf829b441800b4a5a3dddcf83
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-07 10:06:21 +0530

    Update sample.js

--- src/commands/sample/sample.js
+++ src/commands/sample/sample.js
@@ -9,7 +9,7 @@ module.exports = {
     description: "desc", //Description of that command, Optional
     permissions: [], //Permissions required by a member to execute this command
     args: false, //Is at least 1 argument required? Optional
-    catchError: false, //Do you want to catch error, log it and reply that there was an error
+    catchError: false, //Do you want to catch error, log it and reply that there was an error? Optional
     usage: "[arg1] (arg2)", //How to use this command? Optional. Here arg1 is required and arg2 is optional.
     disabled: true, //Is this command disabled? Optional
     subcommand: false, //Is at least 1 subcommand required? Optional
