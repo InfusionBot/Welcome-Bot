24ad0eba106214fc77c5f042075900d7950427ab
PuneetGopinath <baalkrshna@gmail.com>
2021-06-07 08:49:04 +0530

    Add moderation command (not complete)

---
+++ src/commands/mod/ban.js
@@ -0,0 +1,16 @@
+/**
+ * Discord Welcome bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+module.exports = {
+    name: "ban",
+    //aliases: [],
+    description: "Ban a user.",
+    permissions: ["MANAGE_SERVER"],
+    args: true,
+    usage: "[@mention] (reason)",
+    execute(message, args) {
+        const getUserFromMention = require("../../functions/getUserFromMention.js");
+    },
+};
