3056551b951c8d52184767eb46770ca652ba5f66
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-07 10:32:03 +0530

    Update sample.js

--- src/commands/sample/sample.js
+++ src/commands/sample/sample.js
@@ -7,7 +7,8 @@ module.exports = {
     name: "sample", //Command name, Required
     aliases: ["example"], //Aliases for that command, Optional
     description: "desc", //Description of that command, Optional
-    permissions: [], //Permissions required by a member to execute this command
+    permissions: [], //Permissions required by a member to execute this command, Optional
+    bot_perms: [], //Permissions required by bot to execute this command, Optional
     args: false, //Is at least 1 argument required? Optional
     catchError: false, //Do you want to catch error, log it and reply that there was an error? Optional
     usage: "[arg1] (arg2)", //How to use this command? Optional. Here arg1 is required and arg2 is optional.
