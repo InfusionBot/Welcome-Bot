48bf553ee260b625fcb47ddb25b21023b99f7c83
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-07 09:39:21 +0530

    Update kick.js

--- src/commands/mod/kick.js
+++ src/commands/mod/kick.js
@@ -7,10 +7,33 @@ module.exports = {
     name: "kick",
     //aliases: [],
     description: "Kick a user.",
-    permissions: ["MANAGE_SERVER"],
+    permissions: ["BAN_MEMBERS"],
     args: true,
     usage: "[@mention] (reason)",
     execute(message, args) {
         const getUserFromMention = require("../../functions/getUserFromMention.js");
+        if (args.length < 1) {
+            //`args.length < 2` if reason is required
+            return message.reply(
+                "Please mention the user you want to kick (required) and specify a kick reason (optional)."
+            );
+        }
+
+        const user = getUserFromMention(args[0]);
+        if (!user) {
+            return message.reply(
+                "Please use a proper mention if you want to kick someone."
+            );
+        }
+
+        const reason = args.slice(1).join(" ");
+        try {
+            if (reason) user.kick(reason);
+            else user.kick(reason)
+        } catch (error) {
+            return message.channel.send(
+                `Failed to kick **${user.tag}**: ${error}`
+            );
+        }
     },
 };
