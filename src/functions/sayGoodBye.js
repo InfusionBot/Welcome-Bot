4b960ab7fb0db3b7d7eae300f5061e5827c7a80c
Puneet Gopinath <baalkrshna@gmail.com>
2021-06-09 07:04:15 +0530

    Create sayGoodBye.js

---
+++ src/functions/sayGoodBye.js
@@ -0,0 +1,24 @@
+/**
+ * Discord Welcome bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+const getGuild = require("../db/functions/getGuild");
+module.exports = async (guild, member) => {
+    let guildDB = await getGuild(guild.id);
+    let channel = guild.channels.cache.find(
+        (ch) => ch.name === guildDB.welcomeChannel
+    );
+    if (!channel) {
+        return;
+    }
+    channel.startTyping(1);
+    let msg = guildDB.goodByeMessage;
+    //Replace Placeholders with their values
+    msg = msg
+        .replace("{mention}", `${member}`)
+        .replace("{server}", `${guild.name}`)
+        .replace("{members}", `${guild.memberCount}`);
+    channel.send(msg);
+    channel.stopTyping();
+};
