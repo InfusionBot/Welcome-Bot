649d09bd15b83ae6076f4da355b44a7effa1321c
PuneetGopinath <baalkrshna@gmail.com>
2021-06-13 19:29:51 +0530

    commands: new command pokemon

---
+++ src/commands/fun/pokemon.js
@@ -0,0 +1,23 @@
+/**
+ * Discord Welcome bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+module.exports = {
+    name: "pokemon",
+    description: "Get a pokemon image",
+    cooldown: 10,
+    args: true,
+    usage: "[pokemon character]",
+    category: "Fun",
+    async execute(message, args) {
+        const { MessageEmbed } = require("discord.js");
+        const randomImage = require("../../functions/randomImage.js");
+        const url = await randomImage(args[0]);
+        if (url.startsWith("http")) {
+            let image = new MessageEmbed();
+            image.setImage(url);
+            message.channel.send(image);
+        }
+    },
+};
--- src/functions/randomImage.js
+++ src/functions/randomImage.js
@@ -4,8 +4,8 @@
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
 const pokemon = require("pokemon");
-module.exports = function () {
-    let imageId = pokemon.getId(pokemon.random());
+module.exports = function (random=true) {
+    let imageId = pokemon.getId(random===true?pokemon.random():random);
     let imageUrl = `https://pokeres.bastionbot.org/images/pokemon/${imageId}.png`;
     return new Promise(function (resolve, reject) {
         if (imageUrl) {
