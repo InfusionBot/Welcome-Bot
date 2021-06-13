ee5c12b670964fdf0bbcb6990c658a9c64222a12
PuneetGopinath <baalkrshna@gmail.com>
2021-06-13 21:43:19 +0530

    Small bug fix

--- src/functions/randomImage.js
+++ src/functions/randomImage.js
@@ -4,8 +4,18 @@
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
 const pokemon = require("pokemon");
+//https://stackoverflow.com/a/4878800
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
 module.exports = function (random = true) {
-    let imageId = pokemon.getId(random === true ? pokemon.random() : random);
+    if (random === true)
+        random = pokemon.random();
+    else
+        random = toTitleCase(random);
+    let imageId = pokemon.getId(random);
     let imageUrl = `https://pokeres.bastionbot.org/images/pokemon/${imageId}.png`;
     return new Promise(function (resolve, reject) {
         if (imageUrl) {
