154ec5ef924e9427e8f46cf7737ac812bfa29d3a
PuneetGopinath <baalkrshna@gmail.com>
2021-06-14 07:31:37 +0530

    reply for wrong

--- src/functions/randomImage.js
+++ src/functions/randomImage.js
@@ -13,13 +13,14 @@ function toTitleCase(str) {
 module.exports = function (random = true) {
     if (random === true) random = pokemon.random();
     else random = toTitleCase(random);
-    let imageId = pokemon.getId(random);
-    let imageUrl = `https://pokeres.bastionbot.org/images/pokemon/${imageId}.png`;
     return new Promise(function (resolve, reject) {
-        if (imageUrl) {
+        let imageId = pokemon.getId(random).catch(err => {
+            console.error(err);
+            return reject(err);
+        });
+        let imageUrl = `https://pokeres.bastionbot.org/images/pokemon/${imageId}.png`;
+        if (imageUrl && imageUrl.startsWith("http")) {
             resolve(imageUrl);
-        } else {
-            reject("Can't get image");
         }
     });
 };
