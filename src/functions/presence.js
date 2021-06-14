0840879b34d59bb2c89c487a0e0fecfe02a1e71f
PuneetGopinath <baalkrshna@gmail.com>
2021-06-14 18:42:48 +0530

    presence

--- src/functions/presence.js
+++ src/functions/presence.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
-module.exports = function (client) {
+module.exports = async function (client) {
     const servers = client.guilds.cache.size;
     const chans = client.channels.cache.size;
     const users = client.users.cache.size;
