1186f287b893fca8dcb1a040e421f5cae1fc9a6d
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2021-06-15 12:17:19 +0530

    Update versionSender.js

--- src/functions/versionSender.js
+++ src/functions/versionSender.js
@@ -9,7 +9,7 @@ module.exports = async (client) => {
             if (err) {
                 console.log(err);
             } else {
-                let msg = `New Version: **${client.botVersion}**`;
+                let reply = `New Version: **${client.botVersion}**`;
                 client.changelog.forEach((change) => {
                     if (change.startsWith("**")) {
                         reply += `\n${change}`;
@@ -17,17 +17,17 @@ module.exports = async (client) => {
                         reply += `\n- ${change}`;
                     }
                 });
-                let guildDB;
+
                 guilds.forEach((guild) => {
                     if (!guild.unsubscribed) {
                         let clientGuild = client.guilds.cache.get(
                             guild.guildId
                         );
-                        guildDB = getGuild(guild.guildId);
-                        let reply =
-                            msg +
-                            `\n\nIf you want to unsubscribe from these version updates, send \`${guildDB.prefix}version unsubscribe\``;
+
+                        reply += `\n\nIf you want to unsubscribe from these version updates, send \`${guild.prefix} version unsubscribe\``;
+
                         let systemChanel = clientGuild.systemChannelID;
+
                         clientGuild.channels.cache
                             .get(systemChanel)
                             .send(reply);
