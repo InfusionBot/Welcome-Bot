e18fef284bf5a1f3f489d01868b17cc083706bf4
PuneetGopinath <baalkrshna@gmail.com>
2021-07-01 15:29:35 +0530

    don't debug locale loader

--- src/loaders/Locale.js
+++ src/loaders/Locale.js
@@ -29,7 +29,7 @@ module.exports = async (client, dirPath = __dirname + "/../locales") => {
                 interpolation: {
                     escapeValue: false,
                 },
-                debug: client.debug,
+                //debug: client.debug,
                 returnEmptyString: false,
             },
             () => {
