f2676c9b12bb2a1541c7b0079186c992bb65c48b
PuneetGopinath <baalkrshna@gmail.com>
2021-07-05 17:21:35 +0530

    Fix bug

--- src/classes/Embed.js
+++ src/classes/Embed.js
@@ -7,6 +7,7 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = class Embed extends MessageEmbed {
     constructor({tag, avatarURL, color, timestamp}) {
+        super();
         switch (color.toLowerCase()) {
             case "error":
                 color = "#ff0000";
