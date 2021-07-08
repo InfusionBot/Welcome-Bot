2d3b892229661d81d5efeba784e04a75e8706827
PuneetGopinath <baalkrshna@gmail.com>
2021-07-08 11:52:00 +0530

    Another fix

--- src/shard.js
+++ src/shard.js
@@ -6,7 +6,7 @@
 //This file will be used in future
 const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager(__dirname + "/bot.js", {
-    token: process.env.BOT_TOKEN,
+    token: process.env.DISCORD_TOKEN,
 });
 
 manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
