fec7d9ec54377abf65292931e87aaba1077da468
PuneetGopinath <baalkrshna@gmail.com>
2021-07-13 12:53:46 +0530

    refactor(version cmd): Use embeds for version cmd

--- src/commands/general/version.js
+++ src/commands/general/version.js
@@ -3,6 +3,8 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
+const getVersion = require("../../db/functions/version/getVersion.js");
+const { Embed } = require("../../classes");
 module.exports = {
     name: "version",
     aliases: ["ver"],
@@ -11,13 +13,12 @@ module.exports = {
     usage: "(version)",
     cooldown: 10,
     category: "General",
-    async execute(message, args, guildDB) {
-        const getVersion = require("../../db/functions/version/getVersion.js");
-
+    async execute(message, args, guildDB, t) {
         if (!args[0]) args[0] = message.client.botVersion;
         if (args[0].startsWith("v")) {
             args[0] = args[0].replace("v", "");
         }
+        const embed = new Embed({color: "lightblue", tag: message.author.tag});
         let log = await getVersion(args[0].trim() || message.client.botVersion);
         let reply;
         if (log) {
@@ -32,6 +33,9 @@ module.exports = {
         } else {
             reply = `Version \`${args[0]}\` does not exist or is very old! Latest version is: \`v${message.client.botVersion}\``;
         }
-        message.channel.send(reply);
+        embed
+            .setTitle(t("cmds:version.cmdDesc"))
+            .setDesc(reply);
+        message.reply({embeds: [embed]});
     },
 };
