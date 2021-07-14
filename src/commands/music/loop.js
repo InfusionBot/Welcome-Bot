70ecb4bec59f5cd8d7339cbaea352e0b86ebe0fd
PuneetGopinath <baalkrshna@gmail.com>
2021-07-14 06:37:39 +0530

    fix: Embed is not defined

--- src/commands/music/loop.js
+++ src/commands/music/loop.js
@@ -4,6 +4,7 @@
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
 const { QueueRepeatMode } = require("discord-player");
+const { Embed } = require("../../classes");
 module.exports = {
     name: "loop",
     aliases: ["setloop"],
