50bb6ff5fc92715c2b607c4fc53fb0901fb6e696
PuneetGopinath <baalkrshna@gmail.com>
2021-07-15 11:36:21 +0530

    tests: Add new tests
    - Commands
        - should be defined in cmds.json
        - should have proper category name
    
    Last time I had made a typo when typing category name for addemoji cmd
    Theses tests will avoid those problems

--- src/test/commands.js
+++ src/test/commands.js
@@ -6,6 +6,7 @@
 const WelcomeBot = require("../WelcomeBot");
 const client = new WelcomeBot();
 const commands = client.commands.enabled;
+require("../loaders/Locale.js")(client).then(success => console.log("Locales are " + (success ? "loaded" : "not loaded")));
 //findArrDups is took from https://flexiple.com/find-duplicates-javascript-array/
 const findArrDups = (array) => {
     //find duplicates in an array
@@ -29,6 +30,56 @@ describe("Commands", () => {
                     `Some of them have duplicate names or aliases, they are: ${duplicates.join(
                         ", "
                     )}`
+                )
+            );
+        }
+    });
+
+    it("should be defined in cmds.json", (done) => {
+        const t = client.i18next.getFixedT("en-US");
+        const cmds = commands.reduce((arr, command) => {
+            const { name } = command;
+            return [...arr, name];
+        }, []);
+        let errors = [];
+        for (var i = 0; i < cmds.length; i++) {
+            if (t(`cmds:${cmds[i]}.cmdDesc`) === `${cmds[i]}.cmdDesc`) {
+                errors.push(cmds[i]);
+            }
+        }
+        if (!errors.length) {
+            done();
+        } else {
+            done(
+                new Error(
+                    `Some of the cmds are not defined in cmds.json, they are ${errors.join(", ")}`
+                )
+            );
+        }
+    });
+
+    it("should have proper category name", (done) => {
+        const { categories } = client;
+        let categoryNames = [];
+        for (var i = 0; i < categories.length; i++) {
+            categoryNames.push(categories[i].name);
+        }
+        const cmdCats = commands.reduce((arr, command) => {
+            const { category } = command;
+            return [...arr, category];
+        }, []);
+        let errors = [];
+        for (var i = 0; i < cmdCats.length; i++) {
+            if (!categoryNames.has(cmdCats[i])) {
+                errors.push(cmdCats[i]);
+            }
+        }
+        if (!errors.length) {
+            done();
+        } else {
+            done(
+                new Error(
+                    `Some of the cmds have wrong category name, they are ${errors.join(", ")}`
                 )
             );
         }
