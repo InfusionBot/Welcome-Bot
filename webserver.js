e91f4a497cd1332178a76774c7b89a9f0686114e
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-15 17:39:18 +0530

    fix: status func is depreciated
    So switch to sendStatus

--- webserver.js
+++ webserver.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
-
 const express = require("express");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.status(200);
+    res.sendStatus(200);
 });
 
 const port = process.env.PORT || 3000;
