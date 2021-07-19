40749bdb212bc1acd2dae39668f6a4ee78807feb
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-19 20:23:11 +0530

    refactor(disable cmd): Disable commands using disable cmd now!
    Changes
    - Rename subcmd `show` to `display`
    - Use embed for displaying current settings

--- src/commands/setup/disable.js
+++ src/commands/setup/disable.js
@@ -1,9 +1,10 @@
 /**
- * Discord Welcome bot
+ * Discord Welcome-Bot
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
 const { Permissions } = require("discord.js");
+const updateGuild = require("../../db/functions/guild/updateGuild");
 const { Embed, Command } = require("../../classes");
 module.exports = class CMD extends Command {
     constructor(client) {
@@ -13,14 +14,13 @@ module.exports = class CMD extends Command {
                 memberPerms: [Permissions.FLAGS.MANAGE_GUILD],
                 botPerms: [],
                 requirements: {
-                    subcommand: true,
+                    subcommand: false,
+                    args: false,
                     guildOnly: true,
                 },
-                usage: "[subcommand]",
+                usage: "(command name / subcommand)",
                 subcommands: [
-                    { name: "welcome", desc: "Disable welcome logs" },
-                    { name: "goodbye", desc: "Disable goodBye logs" },
-                    { name: "show", desc: "Show current settings" },
+                    { name: "display", desc: "Show current settings" },
                 ],
                 disabled: false,
                 cooldown: 10,
@@ -31,34 +31,19 @@ module.exports = class CMD extends Command {
     }
 
     async execute({ message, args, guildDB }, t) {
-        const updateGuild = require("../../db/functions/guild/updateGuild");
+        const embed = new Embed({color: "red"})
+            .setTitle(t("cmds:disable.title"));
         args[0] = args[0] ? args[0] : "";
-        switch (args[0].toLowerCase()) {
-            case "welcome":
-                updateGuild(message.guild.id, "enableWelcome", false);
-                message.react("👍");
-                break;
-            case "goodbye":
-                updateGuild(message.guild.id, "enableGoodbye", false);
-                message.react("👍");
-                break;
-            default:
-                if (!args.length) {
-                    return message.channel.send(
-                        `Welcome logs are ${
-                            guildDB.enableWelcome ? "enabled" : "disabled"
-                        }\nAnd goodBye logs are ${
-                            guildDB.enableGoodbye ? "enabled" : "disabled"
-                        }`
-                    );
-                } else {
-                    message.reply(
-                        t("cmds:channel.invalidArgs") +
-                            `${guildDB.prefix}help disable`
-                    );
-                }
-                break;
+        const { disabled } = guildDB;
+        if (!args.length) {
+            return message.reply({embeds: [embed.setDesc(`• ${disabled.join("\n• ")}`)]});
+        } else {
+            const cmd = this.client.commands.enabled.find(args[0].toLowerCase());
+            if (!cmd) {
+                return message.reply(t("errors:commandNotFound"));
+            }
+            updateGuild(message.guild.id, "disabled", [cmd.name, ...disabled]);
+            return message.reply(t("cmds:disable.done"));
         }
-        return;
     }
 };
