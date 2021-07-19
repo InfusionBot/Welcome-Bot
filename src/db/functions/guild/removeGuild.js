016e69349b0112c428f8a9a5e02f0c624526c71c
PuneetGopinath <baalkrshna@gmail.com>
2021-07-19 17:13:13 +0530

    feat: Add disabled field to guildSchema & minor changes

--- src/db/functions/guild/removeGuild.js
+++ src/db/functions/guild/removeGuild.js
@@ -1,5 +1,5 @@
 /**
- * Discord Welcome bot
+ * Discord Welcome-Bot
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
@@ -7,7 +7,7 @@ const Guild = require("../../../schema/guildSchema");
 
 module.exports = (guildId) => {
     return new Promise((resolve, reject) => {
-        Guild.where({ guildId: guildId }).deleteOne((err) => {
+        Guild.where({ guildId }).deleteOne((err) => {
             if (err) {
                 return reject("Could not delete guild");
             } else {
--- src/schema/guildSchema.js
+++ src/schema/guildSchema.js
@@ -1,5 +1,5 @@
 /**
- * Discord Welcome bot
+ * Discord Welcome-Bot
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
@@ -42,20 +42,13 @@ const guildSchema = new mongoose.Schema({
         trim: true,
         default: "mod-log",
     },
-    enableWelcome: {
-        type: Boolean,
-        default: true,
-    },
-    enableGoodbye: {
-        type: Boolean,
-        default: true,
-    },
     lang: {
         type: String,
         required: true,
         trim: true,
         default: "en-US",
     },
+    disabled: [String],
 });
 
 const Guild = new mongoose.model("Guild", guildSchema);
