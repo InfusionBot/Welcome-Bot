3327e3e3e0397884e69ca54ee1c60b0ccb6704c1
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-19 19:25:51 +0530

    feat: New command testgoodbye

---
+++ src/commands/general/testgoodbye.js
@@ -0,0 +1,37 @@
+/**
+ * Discord Welcome-Bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+const { Permissions } = require("discord.js");
+const { Embed, Command } = require("../../classes");
+const sayGoodBye = require("../../functions/sayGoodBye");
+module.exports = class CMD extends Command {
+    constructor(client) {
+        super(
+            {
+                name: "testgoodbye",
+                memberPerms: [Permissions.FLAGS.MANAGE_GUILD],
+                botPerms: [],
+                requirements: {
+                    guildOnly: true,
+                },
+                disabled: false,
+                cooldown: 10,
+                category: "General",
+            },
+            client
+        );
+    }
+
+    execute({ message, args }, t) {
+        const result = sayGoodBye(message.member);
+        if (
+            typeof result === "string" &&
+            result.indexOf("channelNotFound") !== -1
+        ) {
+            message.reply(result);
+        }
+        message.react("👍");
+    }
+};
