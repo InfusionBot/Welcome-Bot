9f732bc7bfcf2d683d007a1723b72274cc2ff281
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-21 15:39:41 +0530

    fix(webserver): Use dashboard

--- webserver.js
+++ webserver.js
@@ -1,16 +1,2 @@
-require("dotenv").config();
-const express = require("express");
-
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.sendStatus(200);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
-});
-
+require("./dashboard/app");
 require("./src/bot");
