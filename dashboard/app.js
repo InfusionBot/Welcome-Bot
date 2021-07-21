b4ff6a4562087e8609e17244c7375c737093febb
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-21 15:44:19 +0530

    fix(app.js): Add copyright

--- dashboard/app.js
+++ dashboard/app.js
@@ -1,3 +1,8 @@
+/**
+ * Discord Welcome-Bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
 require("dotenv").config();
 const express = require("express");
 
