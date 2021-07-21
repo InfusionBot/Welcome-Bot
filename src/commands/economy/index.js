dda4cb0c8576cc027e9ea21b8502a73d07c3147e
PuneetGopinath <baalkrshna@gmail.com>
2021-07-21 07:19:07 +0530

    fix(economy): Change emoji

--- src/commands/economy/index.js
+++ src/commands/economy/index.js
@@ -3,6 +3,7 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
+const emojis = require("../../data/customEmojis.json");
 const commands = require("fs")
     .readdirSync(__dirname)
     .filter((file) => file !== "index.js" && file.endsWith(".js"))
@@ -13,6 +14,6 @@ module.exports = {
     metadata: {
         name: "Economy",
         key: "economy",
-        emoji: "💰",
+        emoji: emojis.economy,
     },
 };
