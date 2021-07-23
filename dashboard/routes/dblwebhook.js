a53d8d5523762dd7cb4139ed8e7222989209e971
PuneetGopinath <baalkrshna@gmail.com>
2021-07-23 21:40:06 +0530

    refactor(VoteLogger): Remove src/loaders/VoteLogger.js & Add dashboard/routes/dblwebhook.js

---
+++ dashboard/routes/dblwebhook.js
@@ -0,0 +1,32 @@
+/**
+ * Discord Welcome-Bot
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+const Topgg = require("@top-gg/sdk");
+const express = require("express");
+const router = express.Router();
+const webhook = new Topgg.Webhook(process.env.DBL_token);
+//POST /dblwebhook
+router.post("/dblwebhook", webhook.listener(async (vote) => {
+    const client = req.client;
+            const dUser = await client.users.fetch(vote.user);
+            await client.userDbFuncs.addUser(dUser.id);
+            const userDB = await client.userDbFuncs.getUser(dUser.id);
+            await client.userDbFuncs.updateUser(
+                dUser.id,
+                "wallet",
+                parseInt(userDB.wallet) + 50
+            ); //Give user 50 coins
+            if (client.config.votesChannelId) {
+                client.channels.cache
+                    .get(client.config.votesChannelId)
+                    .send(
+                        `⬆️ **${dUser.tag}** (\`${dUser.id}\`) voted for **${
+                            client.user.username
+                        }** on top.gg and got 50 wcoins 🎉!`
+                    );
+            }
+        })
+    );
+module.exports = router;
--- src/config.js
+++ src/config.js
@@ -9,16 +9,11 @@ module.exports = {
     newsChannelId: "847459283876577360",
     logsChannelId: "855331801635749888",
     votesChannelId: "867925583777103872",
-    rewardUserOnVote: true,
     suggestionLogsChannelId: "862126837110800414",
     ownerIDs: [
         "815204465937481749" /*PuneetGopinath#0001*/,
         "693754859014324295" /*abhijoshi2k#6842*/,
     ],
-    dbl: {
-        port: 2000,
-        token: process.env.DBL_token,
-    },
     dashboard: {
         port: 8000,
         secret: process.env.SESS_SECRET ?? null,
