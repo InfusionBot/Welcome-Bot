cce5ab520b72c452cecf72f843bdba934c223a70
PuneetGopinath <baalkrshna@gmail.com>
2021-07-28 21:52:08 +0530

    feat(dash): add prompt=none so that if already authorized, it won't ask now

--- dashboard/routes/discord.js
+++ dashboard/routes/discord.js
@@ -21,7 +21,7 @@ router.get("/login", (req, res) => {
                 )}/discord/callback?redirectUrl=${encodeURIComponent(
                     req.query.redirectUrl || "/dashboard"
                 )}`
-            )}&response_type=code&scope=identify%20guilds`
+            )}&response_type=code&scope=identify%20guilds&prompt=none`
         );
 });
 //GET /callback
