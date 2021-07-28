c6b7cabc6461feb77c525a93a6be7cc8d2bcaccc
PuneetGopinath <baalkrshna@gmail.com>
2021-07-28 14:44:48 +0530

    fix(error)

--- dashboard/routes/manage.js
+++ dashboard/routes/manage.js
@@ -14,7 +14,7 @@ router.get("/:guildId", CheckAuth, (req, res) => {
     } catch (e) {
         if (process.env.NODE_ENV === "development") console.error(e);
     }
-    const guild = userData.displayedGuilds.find((g) => g.id === guildId);
+    const guild = req.userData.displayedGuilds.find((g) => g.id === guildId);
     if (!guild) {
         return res.send({ error: "404", message: "Server/Guild not found" });
     }
