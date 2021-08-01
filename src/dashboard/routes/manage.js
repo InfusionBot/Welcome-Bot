066c9df01c77a4c72bf952a1fa0d09046aff2c10
PuneetGopinath <baalkrshna@gmail.com>
2021-08-01 08:00:39 +0530

    fix(dash): fix csrf token error

--- src/dashboard/routes/manage.js
+++ src/dashboard/routes/manage.js
@@ -43,7 +43,7 @@ router.get("/:guildId", CheckAuth, async (req, res) => {
         dclient: req.client,
         translate: req.translate,
         currentURL: req.currentURL,
-        csrfToken: req.session.csrfSecret,
+        csrfToken: req.csrfToken(),
     });
     res.end();
 });
