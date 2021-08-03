3559a8c2cb2337eb061394a9e815524860c5d68e
PuneetGopinath <baalkrshna@gmail.com>
2021-08-03 06:34:05 +0530

    refactor: execule owner commands from docs & help cmd

--- src/WelcomeBot.js
+++ src/WelcomeBot.js
@@ -205,7 +205,7 @@ class WelcomeBot extends Client {
                     process.exit();
                 }
             }
-            this.categories.push(metadata);
+            if (metadata.name.indexOf("Owner") === -1) this.categories.push(metadata);
         }
         if (this.debug && this.debugLevel > 1)
             this.logger.log("Finished loading commands", "debug", [
