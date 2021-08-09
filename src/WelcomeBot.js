a36ff0bcc2742771acc8d3a45849c99d6235fc07
PuneetGopinath <baalkrshna@gmail.com>
2021-08-09 20:01:42 +0530

    fix

--- src/WelcomeBot.js
+++ src/WelcomeBot.js
@@ -31,7 +31,7 @@ class WelcomeBot extends Client {
                 Intents.FLAGS.DIRECT_MESSAGES,
                 Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
                 Intents.FLAGS.GUILD_VOICE_STATES,
-                Intents.FLAGS.GUILD_PRESENCES,
+                //Intents.FLAGS.GUILD_PRESENCES,
             ],
             makeCache: Options.cacheWithLimits({
                 MessageManager: 200,
