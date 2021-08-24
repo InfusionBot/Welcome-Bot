75f1c3c37e1f729a1a29741bed1ba0f0547c2075
PuneetGopinath <baalkrshna@gmail.com>
2021-08-24 17:21:07 +0530

    feat: add fonts for welcome image

--- src/functions/greetUser.js
+++ src/functions/greetUser.js
@@ -7,17 +7,17 @@ const getGuild = require("../db/functions/guild/getGuild");
 const { nth } = require("../helpers/Util.js");
 const { Embed } = require("../classes");
 const { MessageAttachment } = require("discord.js");
+const { resolve } = require("path");
 const Canvas = require("canvas");
+Canvas.registerFont(resolve(__dirname + "/../assets/fonts/theboldfont.ttf"), { family: "Bold" });
+Canvas.registerFont(resolve(__dirname + "/../assets/fonts/JosefinSans-Bold.ttf"), { family: "JosefinSans" });
 // Pass the entire Canvas object because you'll need access to its width and context
-const applyText = (canvas, text) => {
+const applyText = (canvas, text, fontSize=60, font="Bold") => {
     const context = canvas.getContext("2d");
-
-    // Declare a base size of the font
-    let fontSize = 70;
 
     do {
         // Assign the font to the context and decrement it so it can be measured again
-        context.font = `${fontSize -= 10}px sans-serif`;
+        context.font = `${fontSize -= 10}px ${font}`;
         // Compare pixel width of the text to the canvas minus the approximate avatar size
     } while (context.measureText(text).width > canvas.width - 300);
 
@@ -25,6 +25,7 @@ const applyText = (canvas, text) => {
     return context.font;
 };
 module.exports = async (member) => {
+    const { client } = member;
     const guildDB = await getGuild(member.guild.id);
     if (
         !guildDB.plugins.welcome.enabled ||
@@ -45,40 +46,38 @@ module.exports = async (member) => {
         return "channelNotFound";
     }
     channel.sendTyping();
+    const t = client.i18next.getFixedT(guildDB.lang ?? "en-US");
 
     const canvas = Canvas.createCanvas(1024, 450);
     const ctx = canvas.getContext("2d");
-    const background = await Canvas.loadImage(__dirname + "/../assets/img/welcome_bg.jpg");
 
     // Draw the background image
+    const background = await Canvas.loadImage(__dirname + "/../assets/img/welcome_bg.jpg");
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-    // Set the color of the stroke
-    ctx.strokeStyle = "#0099ff";
-
     // Draw a rectangle with the dimensions of the entire canvas
+    ctx.strokeStyle = "#0099ff";
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
     // Add a text "Welcome!" in the top
-    ctx.font = "28px sans-serif";
+    const welcome = t("misc:welcome");
+    ctx.font = applyText(canvas, welcome, 40);
     ctx.fillStyle = "#ffffff";
-    ctx.fillText("Welcome!", canvas.width / 2.5, canvas.height / 3.5);
+    ctx.fillText(welcome, canvas.width / 2.5, canvas.height / 3.5);
 
-    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: "jpg" }));
+    // Add the user's tag in the center
+    ctx.font = applyText(canvas, `${member.user.tag}`, 60, "JosefinSans");
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(`${member.user.tag}`, canvas.width / 2.5, canvas.height / 1.8);
 
-    // Move the image downwards vertically and constrain its height to 200, so that it's square
-    ctx.drawImage(avatar, 25, 25, 200, 200);
-
-    // Make the avatar circle
     ctx.beginPath();
-    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+    ctx.arc(200, 200, 100, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.clip();
 
-    // Add the user's tag in the center
-    ctx.font = applyText(canvas, `${member.user.tag}`);
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(member.user.tag, canvas.width / 2.5, canvas.height / 1.8);
+    // Draw the user's avatar
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: "jpg", size: 512 }));
+    ctx.drawImage(avatar, 100, 100, 200, 200);
 
     const attachment = new MessageAttachment(canvas.toBuffer(), `welcome-${member.user.tag}.png`);
     let msg = guildDB.plugins.welcome.message;
