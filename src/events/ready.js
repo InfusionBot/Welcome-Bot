bcbea154a05662eae734bb29f754e0743eeff5d7
PuneetGopinath <baalkrshna@gmail.com>
2021-08-29 06:56:40 +0530

    fix

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
-const dbAuditor = require("./db/functions/dbAuditor");
+const dbAuditor = require("../db/functions/dbAuditor");
 module.exports = {
     name: "ready",
     once: true,
