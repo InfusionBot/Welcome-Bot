2eb48ea77ca88fbe44817ac3f3315f1dde60606b
PuneetGopinath <baalkrshna@gmail.com>
2021-09-01 10:49:54 +0530

    fix(chatbot)

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -37,7 +37,7 @@ module.exports = {
             const chat = await require("node-fetch")(chatBotUrl).then((res) =>
                 res.json()
             );
-            message.reply(`${formatChat(chat?.cnt)}`);
+            message.reply(`${chat.cnt ? formatChat(chat.cnt) : "Error: No message provided"}`);
         }
         if (client.debug && client.debugLevel > 0)
             client.logger.log("running execute func", "debug");
