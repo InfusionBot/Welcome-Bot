32cb8f56ccaf288672092d815f631651e8b71cef
PuneetGopinath <baalkrshna@gmail.com>
2021-09-05 07:29:37 +0530

    fix

--- src/commands/general/testgoodbye.js
+++ src/commands/general/testgoodbye.js
@@ -6,7 +6,6 @@
 const { Permissions } = require("discord.js");
 //eslint-disable-next-line no-unused-vars
 const { Embed, Command } = require("../../classes");
-const sayGoodBye = require("../../functions/sayGoodBye");
 module.exports = class CMD extends Command {
     constructor(client) {
         super(
@@ -26,7 +25,7 @@ module.exports = class CMD extends Command {
     }
 
     //eslint-disable-next-line no-unused-vars
-    async execute({ message, args }, t) {
+    async execute({ message }, t) const sayGoodBye = require("../../functions/sayGoodBye");
         const result = await sayGoodBye(message.member);
         if (result === "channelNotFound") {
             return message.reply(t("errors:channelDoesntExist"));
