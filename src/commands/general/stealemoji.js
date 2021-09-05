33a96ad3f7cce712616fda45d9f45bb2f68b40bf
PuneetGopinath <baalkrshna@gmail.com>
2021-09-05 09:57:03 +0530

    fix(stealemoji): fix error name is not defined

--- src/commands/general/stealemoji.js
+++ src/commands/general/stealemoji.js
@@ -55,7 +55,7 @@ module.exports = class CMD extends Command {
                                 embed.setTitle(
                                     `${
                                         t("cmds:stealemoji.error", {
-                                            emojiName: `${name}`,
+                                            emojiName: `${emoji.name}`,
                                         }).split("\n")[1]
                                     }`
                                 ),
