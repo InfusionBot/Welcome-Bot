e84f60fbe370cb3fd86fab29b65ebbf03b0eccdf
PuneetGopinath <baalkrshna@gmail.com>
2021-09-13 06:30:01 +0530

    feat: implement vote reminder

--- src/events/ready.js
+++ src/events/ready.js
@@ -13,23 +13,29 @@ module.exports = {
             client.logger.log(
                 `${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`
             );
+        const cron = require("node-cron");
+        cron.schedule("0 0 */12 * * *", () => {
+            client.channels.cache
+                .get(`${client.config.channels.general}`)
+                .send(`<@&${client.config.roles.voteReminder}> Time to vote! Use vote command`);
+        });
         const presence = require("../functions/presence");
         const serverCount = require("../functions/serverCount");
         if (client.config.dashboard.enabled) client.dashboard.load(client);
         else client.logger.log("Dashboard not enabled", "debug");
         presence(client);
-        if (process.env.NODE_ENV === "production") serverCount(client);
         // 1 * 60 * (1 second)
         // Update presence every 1 minute
         setInterval(() => presence(client), 1 * 60 * 1000);
-        // Update server count every 25 minutes if environment is in PRODUCTION
+        // Update server count every 25 minutes
         if (process.env.NODE_ENV === "production")
             setInterval(() => serverCount(client), 25 * 60 * 1000);
         dbAuditor(client);
         //Run dbAuditor every 3 hours
-        setInterval(() => {
-            dbAuditor(client);
-        }, 3 * 60 * 60 * 1000);
+        if (process.env.NODE_ENV === "production")
+            setInterval(() => {
+                dbAuditor(client);
+            }, 3 * 60 * 60 * 1000);
         require("../functions/versionSender")(client);
         if (process.env.NODE_ENV !== "production")
             require("../helpers/updateDocs")(client);
