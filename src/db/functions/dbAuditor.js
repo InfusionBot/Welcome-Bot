2c1042b505047bc7b646e08d4a1933772bea9e29
PuneetGopinath <baalkrshna@gmail.com>
2021-10-18 09:55:36 +0530

    fix(dbAuditor): fix db auditor bug which deletes all guilds from the database

--- src/db/functions/dbAuditor.js
+++ src/db/functions/dbAuditor.js
@@ -3,10 +3,16 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
-module.exports = (client) => {
-    const Guild = client.db.guildSchema;
-    const actualGuilds = client.guilds.cache.map((guild) => guild.id);
-
+module.exports = async (client) => {
+    const Guild = client.models.Guild;
+    let actualGuilds = client.shard
+        ? await client.shard.broadcastEval((c) =>
+              c.guilds.cache.map((g) => g.id)
+          )
+        : client.guilds.cache.map((g) => g.id);
+    if (client.shard) {
+        actualGuilds = [].concat(...actualGuilds);
+    }
     Guild.where({}).find((err, guilds) => {
         if (err) {
             console.log(err);
