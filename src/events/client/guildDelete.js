5b08ad0960bdd37c09180e565d793b5c924d02be
PuneetGopinath <baalkrshna@gmail.com>
2021-11-01 08:12:03 +0530

    fix(guildDelete): fix typos & minor bug fix

--- src/events/client/guildDelete.js
+++ src/events/client/guildDelete.js
@@ -10,25 +10,36 @@ module.exports = {
     async execute(client, guild) {
         //Bot has been kicked or banned in a guild
         if (guild.available) await client.db.deleteGuild(guild.id);
-        const bots = guild.available
-            ? guild.members.cache.filter((m) => m.user.bot).size
-            : null;
+        let members;
+        try {
+            members = await guild.members.fetch();
+        } catch (e) {
+            members = guild.available ? guild.members.cache : null;
+        }
+        const bots =
+            guild.available && members
+                ? members.filter((m) => m.user.bot).size
+                : null;
         const embed = new Embed({ color: "error", timestamp: true })
             .setTitle(`:x: Removed from "${guild?.name}"`)
             .setDescription(`${guild.id}`)
             .addField(
                 "Info",
                 guild.available
-                    ? `Shard: ${guild?.shardId}\nOwner: <@${
-                          guild?.ownerId
-                      }>\nMembers: ${guild?.memberCount}\nHumams: ${Math.round(
-                          (bots / guild?.memberCount) * 100
-                      )}%`
+                    ? `Shard: ${guild?.shardId}\n` +
+                          `Owner: <@${guild?.ownerId}>\n` +
+                          `Members: ${guild?.memberCount}\n` +
+                          `Real Humans: ${Math.round(
+                              (bots / guild?.memberCount) * 100
+                          )}%`
                     : "No info available"
             );
-        client.channels.cache
-            .get(client.config.logsChannelId)
-            .send({ embeds: [embed] });
+        client.channels
+            .fetch(client.config.logsChannelId)
+            .then((channel) => {
+                if (channel) channel.send({ embeds: [embed] });
+            })
+            .catch(() => {});
         if (!guild.available) return;
         //For music
         const player = client.manager.players.get(guild.id);
